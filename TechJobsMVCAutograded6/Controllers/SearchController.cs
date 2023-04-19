7c63ba994b060fa50010eba233e3cdba89fbab32
Nigel Hendricks <noah@Noahs-MacBook-Pro.local>
2023-04-19 10:45:46 -0500

    A3

--- TechJobsMVCAutograded6/Controllers/SearchController.cs
+++ TechJobsMVCAutograded6/Controllers/SearchController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
@@ -17,6 +18,24 @@ public class SearchController : Controller
         return View();
     }
 
+
     // TODO #3 - Create an action method to process a search request and render the updated search views.
+    public IActionResult Results(string searchType, string searchTerm)
+    {
+        List<Job> jobs = new List<Job>();
+        if (searchType.Equals("All") && searchTerm.Equals("View All"))
+        {
+            jobs = JobData.FindAll();
+        }
+        else
+        {
+            jobs = JobData.FindByColumnAndValue(searchType, searchTerm);
+        }
+
+        ViewBag.jobs = jobs;
+        ViewBag.columns = ListController.ColumnChoices;
+        return View("Index"); 
+    }
 }
+
 
